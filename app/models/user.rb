0ff912b8e1b183d2699788e5a56fa675a6e3494b
KouseiImai <kouseiimai16340@gmail.com>
2020-12-09 14:43:26 +0900

    userモデルのバリデーション追加とサインアップ画面のスタイル調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # バリデーション
+  with_options presence: true do
+    validates :gender_id
+    validates :age_id
+    validates :nickname, length: { maximum: 40 }
+    validates :email, uniqueness: true, format: { with: /@./ }
+    validates :password, format: { with: /[a-z\d]{6,}/ }
+    validates :password_confirmation, format: { with: /[a-z\d]{6,}/ }
+  end
+
 end
