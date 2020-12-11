dc1e9209a9cbd930cb66de278054d9b82a8cf438
KouseiImai <kouseiimai16340@gmail.com>
2020-12-11 11:05:45 +0900

    cコード一桁目用モデルファイルの編集

--- app/models/ccode_firstdigit.rb
+++ app/models/ccode_firstdigit.rb
@@ -1,2 +1,14 @@
-class CcodeFirstdigit < ApplicationRecord
+class CcodeFirstdigit < ActiveHash::Base
+  self.data = [
+    { id: 0, name: '一般'},
+    { id: 1, name: '教養'},
+    { id: 2, name: '実用'},
+    { id: 3, name: '専門'},
+    { id: 4, name: '検定教科書・消費税非課税品・その他'},
+    { id: 5, name: '婦人'},
+    { id: 6, name: '学参I（小中）'},
+    { id: 7, name: '学参II（高校）'},
+    { id: 8, name: '児童'},
+    { id: 9, name: '雑誌扱い'},
+  ]
 end
