e1615b55f317008970dd7c27509249fdd74dc3d0
KouseiImai <kouseiimai16340@gmail.com>
2020-12-13 22:19:52 +0900

    sliderに本の登録画像が表示される様に編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
 
   def index
+    @books = Book.all
   end
 
   def new
@@ -19,7 +20,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :publisher, :author, :publication_date, :isbn, :ccode_firstdigit_id, :ccode_seconddigit_id, :ccode_thirddigit_id, :cover, :description, :keyword).merge(user_id: current_user.id)
+    params.require(:book).permit(:title, :image, :publisher, :author, :publication_date, :isbn, :ccode_firstdigit_id, :ccode_seconddigit_id, :ccode_thirddigit_id, :cover, :description, :keyword).merge(user_id: current_user.id)
   end
 
 end
