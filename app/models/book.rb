c56bbbab44a337274c864aba4d708fbdcdd2a772
KouseiImai <kouseiimai16340@gmail.com>
2020-12-13 17:48:23 +0900

    本情報を登録出来る様booksコントローラ、bookモデル修正

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,10 @@
 class Book < ApplicationRecord
-  has_one_attahed :image
-  belongs_to :ccodefirstdigit, :ccodeseconddigit, :ccodethirddigit
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :CcodeFirstdigit
+  belongs_to_active_hash :CcodeSeconddigit
+  belongs_to_active_hash :CcodeThirddigit
+
+
 end
