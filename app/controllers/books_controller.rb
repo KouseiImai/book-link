1bb8f8ef7c0717d1c10906cc2b3a96ea81a35ce1
KouseiImai <kouseiimai16340@gmail.com>
2020-12-15 20:34:51 +0900

    サインインしていない状態だとトップページと詳細ページにしか遷移できない様に記述

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @books = Book.all
@@ -19,6 +20,10 @@ class BooksController < ApplicationController
   end
 
   private
+
+  def move_to_index
+    redirect_to root_path unless user_signed_in?
+  end
 
   def book_params
     params.require(:book).permit(:title, :image, :publisher, :author, :publication_date, :isbn, :ccode_firstdigit_id, :ccode_seconddigit_id, :ccode_thirddigit_id, :cover, :description, :keyword).merge(user_id: current_user.id)
