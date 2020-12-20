73d8ff0acb0c380c0bce380fc6d73039edb6651d
KouseiImai <kouseiimai16340@gmail.com>
2020-12-20 13:55:33 +0900

    Revert "ユーザーマイページの実装"
    This reverts commit ce4558d98dfb493ec9c8c004760ee99b16d58695.

--- app/controllers/users_controller.rb
+++
@@ -1,6 +0,0 @@
-class UsersController < ApplicationController
-  def show
-    @nickname = current_user.nickname
-    @books = current_user.books
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'books#index'
   resources :books
-  resources :users, only: :show
 end
