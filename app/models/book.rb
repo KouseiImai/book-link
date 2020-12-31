ac9ee9c9a233e73585f892c0c0930b8ffe82ca2c
KouseiImai <kouseiimai16340@gmail.com>
2020-12-31 16:12:33 +0900

    コメント機能追加の為、テーブル、モデル作成、アソシエーションの記述

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
   has_one_attached :image
   belongs_to :user
+  has_many   :comments
 
   has_many  :book_keyword_relations, dependent: :destroy
   has_many  :keyword, through: :book_keyword_relations
