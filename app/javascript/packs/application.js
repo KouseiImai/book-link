868e2f09b0512f84aa160eac446479f25af4cbcd
KouseiImai <kouseiimai16340@gmail.com>
2021-01-04 19:02:18 +0900

    Unnecessary code

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,7 +12,6 @@ require("../book_search")
 require("../mood_search")
 require("../keyword_search")
 require("../preview")
-require("../top_display")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
