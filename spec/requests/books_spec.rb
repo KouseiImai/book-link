2fe34bf84dc0906314275b28e5c54c87954c7415
KouseiImai <kouseiimai16340@gmail.com>
2021-01-06 11:27:08 +0900

    stylesheets file bug correction

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe BooksController, type: :request do
 
   describe "GET #index" do
     it "indexアクションにリクエストすると正常にレスポンスが返ってくる" do
-      binding.pry
       get root_path
       expect(response.status).to eq 200
     end
